f25cba121020159d70195b7dfec3db938aee2d2b
anuj-zylotech <anuj.singh@zylotech.com>
2020-09-03 07:53:56 +0530

    added

--- mod.ts
+++ mod.ts
@@ -1,13 +1,10 @@
-interface Emoji {
-  codes: string,
-  name: string,
-  category: string,
-  group: string,
-  subgroup: string
-}
-
-
-export const getMoji = async (): Promise<any> => {
-    const emojiData = await (await fetch(`https://raw.githubusercontent.com/anujsinghwd/getmoji/master/emoji.json`)).json();
+const getEmojiData = async (): Promise<any> => {
+    const emojiData = await (await fetch(`https://raw.githubusercontent.com/anujsinghwd/getmoji/master/emojiData.json`)).json();
     return emojiData;
 };
+
+export const getEmojiCode = async (name: string): Promise<string> => {
+  const data = await getEmojiData();
+  console.log(data);
+  return "Hi";
+}
